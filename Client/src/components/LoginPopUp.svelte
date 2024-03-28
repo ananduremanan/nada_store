5023656a869229c0e2f88adfb5d706c3ecc00595
ananduremanan <ananduor@gmail.com>
2024-03-28 13:21:04 +0530

    Changes in UI

--- Client/src/components/LoginPopUp.svelte
+++ Client/src/components/LoginPopUp.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import { postSET } from '$lib';
-	import Notify from './Notify.svelte';
 
 	let email: string;
 	let password: string;
@@ -23,7 +22,9 @@
 				body: { email: email, password: password }
 			});
 			if (res.status === 'success') {
+				loginError = false;
 				localStorage.setItem('user', JSON.stringify(res));
+				cancel();
 			} else {
 				loginError = true;
 			}
@@ -38,7 +39,6 @@
 <div
 	class="z-10 bg-black text-white p-4 rounded-xl flex flex-col gap-5 w-full m-8 justify-center items-center relative"
 >
-	<Notify showNotification={loginError} />
 	<button
 		class="bg-white text-black w-6 h-6 rounded-full text-center absolute top-1 right-1"
 		on:click={cancel}>x</button
@@ -57,6 +57,9 @@
 		class="p-2 w-full rounded-md outline-0 text-black text-xl"
 		bind:value={password}
 	/>
+	{#if loginError}
+		<div class="text-xs">Wrong User Email or Password</div>
+	{/if}
 	<button
 		class="bg-white text-black p-2 rounded-md w-full text-xl"
 		on:click={() => {
--- Client/src/routes/+layout.svelte
+++ Client/src/routes/+layout.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
 	import '../app.css';
 	import LoginPopUp from '../components/LoginPopUp.svelte';
+	import { onMount } from 'svelte';
+	import md5 from 'md5';
 
 	let isOpen: boolean = false;
+	let avatrImgUrl: string;
+
+	onMount(() => {
+		const getUserImage = async (email: string) => {
+			if (email != '') {
+				const normalizedEmail = typeof email === 'string' ? email.trim().toLowerCase() : '';
+				const hash = md5(normalizedEmail);
+				avatrImgUrl = `https://gravatar.com/avatar/${hash}?s=200`;
+			}
+		};
+
+		let user = localStorage.getItem('user');
+		if (user) {
+			let parsedUserData = JSON.parse(user);
+			getUserImage(parsedUserData.email);
+		}
+	});
 </script>
 
 <nav class="px-8 lg:px-28 py-4 flex justify-between sticky top-0 z-50 bg-white">
 	<a href="/" class="text-lg">Nada Store</a>
 	<div>
-		<button
-			class="bg-black text-white px-2 py-1 rounded-md"
-			on:click={() => {
-				isOpen = !isOpen;
-			}}>Login</button
-		>
+		{#if avatrImgUrl != undefined}
+			<img src={avatrImgUrl} alt="{avatrImgUrl} avatar" class="w-10 h-10 rounded-xl" />
+		{:else}
+			<button
+				class="bg-black text-white px-2 py-1 rounded-md"
+				on:click={() => {
+					isOpen = !isOpen;
+				}}>Login</button
+			>
+		{/if}
 	</div>
 </nav>
 
