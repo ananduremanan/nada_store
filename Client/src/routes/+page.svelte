3f4ce83dd0365bd1433f1baa9b4c472e47da6078
ananduremanan <ananduor@gmail.com>
2024-04-04 14:33:07 +0530

    UI Fix

--- Client/src/routes/+page.svelte
+++ Client/src/routes/+page.svelte
@@ -1,67 +1,48 @@
 <script lang="ts">
 	import { Card, Button, Rating, Badge } from 'flowbite-svelte';
 	import { Carousel } from 'flowbite-svelte';
-
-	const images = [
-		{
-			alt: 'VR VPN',
-			src: '/images/VR_VPN.svg',
-			title: 'sergey-pesterev-tMvuB9se2uQ-unsplash'
-		},
-		{
-			alt: 'Sergey Pesterev',
-			src: '/images/VR_WORLD.svg',
-			title: 'sergey-pesterev-tMvuB9se2uQ-unsplash'
-		}
-	];
 </script>
 
 <section>
-	<Carousel {images} duration={3000} />
-	<div class="mb-8 text-base">Hot In Wearables</div>
-	<div class="grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-4">
-		<Card>
-			<img src="https://flowbite-svelte.com/images/product-1.webp" alt="" />
-			<h2 class="font-bold">Apple Watch Series 3</h2>
-			<Rating rating={3} size={18} class="mt-2" />
-			<div class="w-full flex justify-between items-center mt-2">
-				<h3 class="text-sm">399 SUD</h3>
-				<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
-					>Buy Now</Button
-				>
+	<div class="mb-4 text-base">Hot In Templates</div>
+	<div class="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-4">
+		<Card padding={'none'} class="rounded-3xl">
+			<img src="/images/svelte.svg" alt="" />
+			<div class="p-4 lg:p-6">
+				<h2 class="font-bold">Svelte E-Commerce Web Template</h2>
+				<Rating rating={3} size={18} class="mt-2" />
+				<div class="w-full flex justify-between items-center mt-2">
+					<h3 class="text-sm">399 SUD</h3>
+					<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
+						>Buy Now</Button
+					>
+				</div>
 			</div>
 		</Card>
-		<Card>
-			<img src="https://flowbite-svelte.com/images/product-1.webp" alt="" />
-			<h2 class="font-bold">Apple Watch Series 3</h2>
-			<Rating rating={3} size={18} class="mt-2" />
-			<div class="w-full flex justify-between items-center mt-2">
-				<h3 class="text-sm">399 SUD</h3>
-				<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
-					>Buy Now</Button
-				>
+		<Card padding={'none'} class="rounded-3xl">
+			<img src="/images/SwiftUI.png" alt="" class="object-cover" />
+			<div class="p-4 lg:p-6">
+				<h2 class="font-bold">SwiftUI Component Library</h2>
+				<Rating rating={4.5} size={18} class="mt-2" />
+				<div class="w-full flex justify-between items-center mt-2">
+					<h3 class="text-sm">699 SUD</h3>
+					<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
+						>Buy Now</Button
+					>
+				</div>
 			</div>
 		</Card>
-		<Card>
-			<img src="https://flowbite-svelte.com/images/product-1.webp" alt="" />
-			<h2 class="font-bold">Apple Watch Series 3</h2>
-			<Rating rating={3} size={18} class="mt-2" />
-			<div class="w-full flex justify-between items-center mt-2">
-				<h3 class="text-sm">399 SUD</h3>
-				<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
-					>Buy Now</Button
-				>
-			</div>
-		</Card>
-		<Card>
-			<img src="https://flowbite-svelte.com/images/product-1.webp" alt="" />
-			<h2 class="font-bold">Apple Watch Series 3</h2>
-			<Rating rating={3} size={18} class="mt-2" />
-			<div class="w-full flex justify-between items-center mt-2">
-				<h3 class="text-sm">399 SUD</h3>
-				<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
-					>Buy Now</Button
-				>
+		<Card padding={'none'} class="rounded-3xl">
+			<img src="/images/svelte.svg" alt="" />
+			<div class="p-4 lg:p-6">
+				<h2 class="font-bold">Svelte E-Commerce Web Template</h2>
+				<Rating rating={3} size={18} class="mt-2" />
+				<div class="w-full flex justify-between items-center mt-2">
+					<h3 class="text-sm">399 SUD</h3>
+					<Button class="text-xs p-1 bg-black dark:bg-white dark:text-black md:text-base"
+						>Buy Now</Button
+					>
+				</div>
 			</div>
 		</Card>
 	</div>
